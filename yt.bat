6878b076079eb392b14d9fb1455efb102014525c
flopana <florian@f-erbs.de>
2019-07-17 13:53:28 +0200

    Added functionality for new Menu
    (Ready for Testing)

--- yt.bat
+++ yt.bat
@@ -1,4 +1,7 @@
 @echo off
+Title flopana's yt downloader
+:start
+
 echo.
 echo ###########################
 echo # flopana's yt downloader #
@@ -9,7 +12,20 @@ echo 1. Download video and convert it to mp3.
 echo 2. Download video as mp4.
 
 
-set /p input=Youtube Link:
-youtube-dl.exe -x --audio-format mp3 --audio-quality 0 "%input%"
+set /p input=Option:
+if %input% equ 1(
+    set /p link=Youtube Link:
+    youtube-dl.exe -x --audio-format mp3 --audio-quality 0 "%link%"
+    goto :start
+)
 
-pause
+if %input% equ 2(
+    set /p link=Youtube Link:
+    youtube-dl "%link%" 
+    goto :start
+)else(
+    echo "Invalid input!"
+    goto :start
+)
+
+pause > nul
