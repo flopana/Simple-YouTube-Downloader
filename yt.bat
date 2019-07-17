d789dfd9be23bad4e3c08aa855e4807a77b3f0d8
flopana <florian@f-erbs.de>
2019-07-17 13:46:08 +0200

    Added menu Choices

--- yt.bat
+++ yt.bat
@@ -4,6 +4,10 @@ echo ###########################
 echo # flopana's yt downloader #
 echo ###########################
 echo.
+echo.
+echo 1. Download video and convert it to mp3.
+echo 2. Download video as mp4.
+
 
 set /p input=Youtube Link:
 youtube-dl.exe -x --audio-format mp3 --audio-quality 0 "%input%"
