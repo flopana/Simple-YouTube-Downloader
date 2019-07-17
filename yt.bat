89684662129e7591cfd7bd4f6da6a3fd6b80017f
flopana <florian@f-erbs.de>
2019-07-17 13:59:31 +0200

    debug

--- yt.bat
+++ yt.bat
@@ -13,8 +13,9 @@ echo 2. Download video as mp4.
 echo.
 
 set /p input=Option:
-
+echo %input%
 IF %input% == 1(
+    echo hallo
     set /p link=Youtube Link:
     youtube-dl.exe -x --audio-format mp3 --audio-quality 0 "%link%"
     goto :start
